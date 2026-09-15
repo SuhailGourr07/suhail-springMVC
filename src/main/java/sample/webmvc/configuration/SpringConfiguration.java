02966e0b6a84f9ad1f9a6fe5f0a5fba0818a99c0
Mohd Suhail <suhailshoib519@gmail.com>
2026-09-15 14:49:11 +0530

    update

--- src/main/java/sample/webmvc/configuration/SpringConfiguration.java
+++ src/main/java/sample/webmvc/configuration/SpringConfiguration.java
@@ -1,16 +1,20 @@
 package sample.webmvc.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "sample.webmvc")
 public class SpringConfiguration implements WebMvcConfigurer{
 
+	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		//it specifies that JSTL views should be used.
